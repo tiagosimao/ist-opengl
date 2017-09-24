703b6656ea81a00d9f7142b1c2868bbbe6157c41
Tiago Simão <tiagosimao@gmail.com>
2017-09-24 23:42:52 +0100

    working with opengl 3.3+

--- app.cpp
+++ app.cpp
@@ -14,18 +14,21 @@ auto logger = spdlog::stdout_color_mt("main");
 
 static void error_callback(int error, const char* description)
 {
-    logger->info("Error %s", description);
+    logger->info("Error {}", description);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        glfwSetWindowShouldClose(win, GLFW_TRUE);
 }
 
-int createWindow(){
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+int createWindow()
+{
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
   glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
   glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
@@ -40,10 +43,20 @@ int createWindow(){
   glfwSetKeyCallback(window, key_callback);
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
+
+  const GLubyte *renderer = glGetString(GL_RENDERER);
+  const GLubyte *vendor = glGetString(GL_VENDOR);
+  const GLubyte *version = glGetString(GL_VERSION);
+  const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+  logger->info("OpenGL Renderer:  ({})", renderer, vendor);
+  logger->info("OpenGL version: {}", version);
+  logger->info("GLSL version: {}", glslVersion);
+
   return 1;
 }
 
-void loop(){
+void loop()
+{
   while (!glfwWindowShouldClose(window))
   {
     glfwSwapBuffers(window);
